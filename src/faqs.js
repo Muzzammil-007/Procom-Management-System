e481740697bc2ff3ab6a255070d5dd5605fe1b55
qasimfuzail <qasimfuzail@gmail.com>
2023-05-03 00:53:52 +0500

    Added FAQs page and Company Registration page

---
+++ src/faqs.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './FAQsPage.css';
+
+const questionsAndAnswers = [
+  {
+    question: 'What is the event about?',
+    answer: 'The event is about showcasing the latest technologies and innovations in the industry.',
+  },
+  {
+    question: 'How can I register for the event?',
+    answer: 'You can register for the event by filling out the registration form on our website.',
+  },
+  {
+    question: 'What are the dates for the event?',
+    answer: 'The event will be held from June 15th to June 17th.',
+  },
+  {
+    question: 'Is there a cost to attend the event?',
+    answer: 'No, attendance to the event is free.',
+  },
+  {
+    question: 'Can I bring a guest to the event?',
+    answer: 'No, the event is only open to registered attendees.',
+  },
+];
+
+const FAQ = ({ question, answer }) => {
+  return (
+    <div className="faq">
+      <div className="faqQuestion">{question}</div>
+      <div className="faqAnswer">{answer}</div>
+    </div>
+  );
+};
+
+const FAQsPage = () => {
+  return (
+    <div>
+    <div className="navbar">
+        <div className="logo">Procom</div>
+        <div className="links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+    <div className="faqsPage">
+      
+      <div className="faqsHeader">Frequently Asked Questions</div>
+      <div className="faqs">
+        {questionsAndAnswers.map((qa, index) => (
+          <FAQ key={index} question={qa.question} answer={qa.answer} />
+        ))}
+      </div>
+    </div></div>
+  );
+};
+
+export default FAQsPage;
---
+++ src/vendors.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './Vendors.module.css';
+
+const Companies = () => {
+    const [companies, setCompanies] = useState([]);
+    const [companyName, setCompanyName] = useState('');
+    const [companyEmail, setCompanyEmail] = useState('');
+    const [companyReps, setCompanyReps] = useState('');
+    const [representatives, setRepresentatives] = useState([]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('/api/companies', {
+                name: companyName,
+                email: companyEmail,
+                reps: companyReps,
+                representatives: representatives
+            });
+            setCompanies([...companies, response.data]);
+            setCompanyName('');
+            setCompanyEmail('');
+            setCompanyReps('');
+            setRepresentatives([]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleAddRepresentative = (event) => {
+        event.preventDefault();
+        setRepresentatives([...representatives, '']);
+    };
+
+    const handleRepresentativeChange = (event, index) => {
+        const updatedRepresentatives = [...representatives];
+        updatedRepresentatives[index] = event.target.value;
+        setRepresentatives(updatedRepresentatives);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/api/companies');
+                setCompanies(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+ <div className="navbar">
+        <div className="logo">Procom</div>
+        <div className="links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+        
+        <div className={styles.container}>
+            <h1 className={styles.heading}>Company Registeration</h1>
+            <ul className={styles.list}>
+                {companies.map((company) => (
+                    <li key={company.id} className={styles.item}>
+                        <h2 className={styles.companyName}>{company.name}</h2>
+                        <p className={styles.companyInfo}>{company.email} - {company.reps} reps</p>
+                        <ul className={styles.representativesList}>
+                            {company.representatives.map((representative) => (
+                                <li key={representative} className={styles.representative}>{representative}</li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <h2 className={styles.formHeading}>Register Your Company</h2>
+                <div className={styles.formField}>
+                    <label className={styles.formLabel}>Name:</label>
+                    <input type="text" value={companyName} onChange={(event) => setCompanyName(event.target.value)} className={styles.formInput} />
+                </div>
+                <div className={styles.formField}>
+                    <label className={styles.formLabel}>Email:</label>
+                    <input type="email" value={companyEmail} onChange={(event) => setCompanyEmail(event.target.value)} className={styles.formInput} />
+                </div>
+                <div className={styles.formField}>
+                    <label className={styles.formLabel}>Number of Representatives:</label>
+                    <input type="number" value={companyReps} onChange={(event) => setCompanyReps(event.target.value)} className={styles.formInput} />
+                </div>
+                {representatives.map((representative, index) => (
+                    <div className={styles.formField} key={index}>
+                        <label className={styles.formLabel}>Representative #{index + 1} Name:</label>
+                        <input type="text" value={representative} onChange={(event) => handleRepresentativeChange(event, index)} className={styles.formInput} />
+                    </div>
+                ))}
+                <button onClick={handleAddRepresentative} className={styles.addButton}>+ Add Representative</button>
+                <button type="submit" className={styles.submitButton}>Register</button>
+            </form>
+        </div></div>
+    );
+};
+
+export default Companies;
+
+
